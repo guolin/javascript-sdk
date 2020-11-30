6a0ec85d1773e6d81df7a575ec28008b6b685a1f
sdjdd <i@sdjdd.com>
2020-11-30 18:57:05 +0800

    refactor: operations

--- src/object/operation.ts
+++ src/object/operation.ts
@@ -1,70 +1,55 @@
 import { pointer } from './pointer';
 import { ensureArray } from '../utils';
 
-export function unset(): any {
+export function unset() {
   return { __op: 'Delete' };
 }
 
-export function increment(amount = 1): any {
+export function increment(amount = 1) {
   return { __op: 'Increment', amount };
 }
 
-export function decrement(amount = 1): any {
+export function decrement(amount = 1) {
   return { __op: 'Decrement', amount };
 }
 
-export function add(objects: any | any[]): any {
+export function add(objects: any | any[]) {
   return { __op: 'Add', objects: ensureArray(objects) };
 }
 
-export function addUnique(objects: any | any[]): any {
+export function addUnique(objects: any | any[]) {
   return { __op: 'AddUnique', objects: ensureArray(objects) };
 }
 
-export function remove(objects: any | any[]): any {
+export function remove(objects: any | any[]) {
   return { __op: 'Remove', objects: ensureArray(objects) };
 }
 
-export function bitAnd(value: number): any {
+export function bitAnd(value: number) {
   return { __op: 'BitAnd', value };
 }
 
-export function bitOr(value: number): any {
+export function bitOr(value: number) {
   return { __op: 'BitOr', value };
 }
 
-export function bitXor(value: number): any {
+export function bitXor(value: number) {
   return { __op: 'BitXor', value };
 }
 
-export function addRelation(
-  objects:
-    | {
-        className: string;
-        objectId: string;
-      }
-    | {
-        className: string;
-        objectId: string;
-      }[]
-): any {
+interface LCObjectInfo {
+  className: string;
+  objectId: string;
+}
+
+export function addRelation(objects: LCObjectInfo | LCObjectInfo[]) {
   return {
     __op: 'AddRelation',
     objects: ensureArray(objects).map((object) => pointer(object)),
   };
 }
 
-export function removeRelation(
-  objects:
-    | {
-        className: string;
-        objectId: string;
-      }
-    | {
-        className: string;
-        objectId: string;
-      }[]
-): any {
+export function removeRelation(objects: LCObjectInfo | LCObjectInfo[]) {
   return {
     __op: 'RemoveRelation',
     objects: ensureArray(objects).map((object) => pointer(object)),
